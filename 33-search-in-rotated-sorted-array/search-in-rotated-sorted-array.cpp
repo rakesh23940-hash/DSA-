b1a08a9b2d45b160f01488eec91d26e4fe1c85d3
RAKESH KUMAR <rakesh23940@gmail.com>
2026-05-27 15:56:59 +0530

    Time: 2 ms (2.46%) | Memory: 15.1 MB (95.98%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int guess = (low + high) / 2;
+
+            if (nums[guess] == target) {
+                return guess;
+            }
+
+           
+            if (nums[low] <= nums[guess]) {
+
+                if (nums[low] <= target && target < nums[guess]) {
+                    high = guess - 1;
+                }
+                else {
+                    low = guess + 1;
+                }
+            }
+
+           
+            else {
+
+                if (nums[guess] < target && target <= nums[high]) {
+                    low = guess + 1;
+                }
+                else {
+                    high = guess - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
